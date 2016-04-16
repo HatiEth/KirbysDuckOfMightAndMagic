81c9701ce34314fd84c089333ed37b816d167d2f
hollowspecter <tigervb@aol.com>
2016-04-16 22:54:38 +0200

    Credits Done

---
+++ Assets/WarningScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class WarningScreen : MonoBehaviour {
+
+    [SerializeField]
+    private float endTime = 5f;
+
+    private float timer = 0f;
+
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if (timer > endTime) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+	}
+}
