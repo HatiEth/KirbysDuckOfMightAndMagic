6b89d211f1007059f32c4975d0282dd4ebcb21db
Hati Eth <hati.eth@gmail.com>
2016-04-16 17:47:49 +0200

    knight follow behaviour

---
+++ Assets/Scripts/BreadsCivil/Knight/BreadKnightBehaviour.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using System.Collections;
+
+public class BreadKnightBehaviour : MonoBehaviour {
+
+	public bool Alarmed = false;
+
+	private NavMeshAgent Agent;
+	private Transform Player;
+
+	Vector3 TargetPosition;
+
+	BreadKnightMovement movement;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		Player = GameObject.FindGameObjectWithTag("Player").transform;
+		Agent = GetComponent<NavMeshAgent>();
+		movement = GetComponent<BreadKnightMovement>();
+
+		Agent.updateRotation = false;
+
+		TargetPosition = Player.position;
+
+	}
+
+	void Update()
+	{
+		/*
+		float alpha = Random.Range(0f, 360f);
+		Vector3 onCircle = new Vector3(Mathf.Cos(alpha), 0.0f, Mathf.Sin(alpha));
+		*/
+		Vector3 onCircle = Vector3.zero;
+
+		//TargetPosition = Vector3.Slerp(TargetPosition, Player.position + onCircle * 0.1f, Time.deltaTime);
+		TargetPosition = Player.position;
+
+		Agent.SetDestination(TargetPosition);
+
+		if(Agent.remainingDistance > Agent.stoppingDistance)
+		{
+			Vector3 v = Agent.desiredVelocity.normalized;
+			movement.Move(v.x, v.z);
+		}
+		else
+		{
+			movement.Move(0f, 0f);
+		}
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+
+		if (Agent.desiredVelocity.magnitude != 0f)
+		{
+
+		}
+	}
+
+	public void OnDrawGizmos()
+	{
+
+		Gizmos.color = Color.red;
+
+		if(Agent != null)
+		{
+			Gizmos.DrawLine(transform.position, transform.position + Agent.desiredVelocity);
+		}
+
+	}
+}
---
+++ Assets/Scripts/BreadsCivil/Knight/BreadKnightMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class BreadKnightMovement : MonoBehaviour{
+
+	Animator anim;
+	SpriteRenderer sprite;
+
+	// Use this for initialization
+	protected	void Start () {
+		var SpriteObj = transform.FindChild("BreadKnightSprite");
+		anim = SpriteObj.GetComponent<Animator>();
+		sprite = SpriteObj.GetComponent<SpriteRenderer>();
+	}
+
+	public void Move(float h, float v)
+	{
+		bool walking = h != 0f || v != 0f;
+
+		anim.SetBool("bWalk", walking);
+		anim.SetFloat("fSpeedX", h, 0.1f, Time.deltaTime);
+		anim.SetFloat("fSpeedY", v, 0.1f, Time.deltaTime);
+		if(h != 0)
+		{
+				sprite.flipX = h < 0;
+		}
+	}
+
+	void Attack()
+	{
+
+	}
+}
