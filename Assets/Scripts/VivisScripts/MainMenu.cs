f14eed60efc1eac0ee36dd5b8c2542ab89eac89f
hollowspecter <tigervb@aol.com>
2016-04-17 14:08:01 +0200

    Added Music and Sound

--- Assets/Scripts/VivisScripts/MainMenu.cs
+++ Assets/Scripts/VivisScripts/MainMenu.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class MainMenu : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject creditsGO;
     [SerializeField]
     private string startText = "Starten";
     [SerializeField]
@@ -13,11 +17,18 @@ public class MainMenu : MonoBehaviour {
     private string endText = "Beenden";
     [SerializeField]
     private float threshhold = 0.4f;
+    [SerializeField]
+    private AudioClip selectedClip;
+    [SerializeField]
+    private Vector2 randomPitch;
 
+    private new AudioSource audio;
+    private GameObject title;
     private Text[] texts;
     private string[] strings;
     private int selector = 0;
     private bool hasSelected = false;
+    private bool credits = false;
 
     void Awake()
     {
@@ -26,15 +37,53 @@ public class MainMenu : MonoBehaviour {
         texts[1] = transform.GetChild(1).GetComponent<Text>();
         texts[2] = transform.GetChild(2).GetComponent<Text>();
         strings = new string[]{startText, creditsText, endText};
+        title = transform.parent.FindChild("Title").gameObject;
+        Assert.IsNotNull<GameObject>(title);
+        Assert.IsNotNull<GameObject>(creditsGO);
+        audio = GetComponent<AudioSource>();
+        Assert.IsNotNull<AudioSource>(audio);
     }
 
     void Start()
     {
         texts[0].text = ">> " + startText + " <<";
+        creditsGO.SetActive(false);
     }
 
     void Update()
     {
+        if (Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Return)) {
+            if (!credits) {
+                switch (selector) {
+                    case 0: SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); break;
+                    case 1:
+                        title.SetActive(false);
+                        credits = true;
+                        creditsGO.SetActive(true);
+                        for (int i = 0; i < 3; ++i) {
+                            texts[i].enabled = false;
+                        }
+                            break;
+                    case 2: Application.Quit(); break;
+                }
+            }
+            else {
+                title.SetActive(true);
+                credits = false;
+                creditsGO.SetActive(false);
+                for (int i = 0; i < 3; ++i) {
+                    texts[i].enabled = true;
+                }
+            }
+
+            // Play Sound
+            PlaySound();
+        }
+
+        if (credits == true)
+            return;
+
+
         float v = Input.GetAxis("Vertical");
         Debug.Log("Vertical: " + v);
 
@@ -62,12 +111,13 @@ public class MainMenu : MonoBehaviour {
         hasSelected = true;
         texts[selector].text = ">> " + strings[selector] + " <<";
 
-        if (Input.GetButtonDown("Fire") || Input.GetKeyDown(KeyCode.Return)) {
-            switch (selector) {
-                case 0: SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); break;
-                case 1: SceneManager.LoadScene(SceneManager.GetSceneByName("Credits").buildIndex); break;
-                case 2: Application.Quit(); break;
-            }
-        }
+        
+    }
+
+    void PlaySound()
+    {
+        float pitch = Random.RandomRange(randomPitch.x, randomPitch.y);
+        audio.pitch = pitch;
+        audio.PlayOneShot(selectedClip);
     }
 }
