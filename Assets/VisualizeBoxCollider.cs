f965b745386d650ee6200f6b5cf36946962420bc
Sushiy <julivizi@yahoo.de>
2016-04-18 15:44:42 +0200

    ´baum weg

---
+++ Assets/VisualizeBoxCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisualizeBoxCollider : MonoBehaviour {
+	#if UNITY_EDITOR
+	void OnDrawGizmos()
+	{
+		Vector3 pos = transform.position + GetComponent<BoxCollider> ().center;
+		Gizmos.color = Color.red;
+		Gizmos.DrawCube (pos, GetComponent<BoxCollider> ().size * transform.lossyScale.x);
+	}
+	#endif
+}
